f2a3f370389dc4f9aed8caede42857b8b82bf8a3
Daffy Duke <garghelladuke@gmail.com>
2025-08-13 12:07:39 +0200

    generalized LLR mse to any dimention dataset

--- Model11/mse_vcm.R
+++ Model11/mse_vcm.R
@@ -54,45 +54,51 @@ asy.var <- function(n,banda,N.matrix,e2,est.density,pesi){
 # h = vettore di bande candidate
 # Z = variabile di smoothing
 
-MSE.LLR <- function(h,data,Z){
+MSE.LLR <- function(h, data, Z) {
   
-  X.matrix <- as.matrix(data[,c('X1','X2','X3')])
+  X <- as.matrix(data[, !names(data) %in% c("Y", "Z")])
+  Y <- data$Y
+  n <- nrow(X)
+  p <- ncol(X)
+  
   MSE <- numeric(length(h))
-  n <- NROW(data)
   
-  for (i in seq_along(h)){
+  for (i in seq_along(h)) {
     
-    LLR.temp <- LLR(Z,data,h[i])
-    bias.temp <- LLR.temp[,c(7:9)]*(h[i]^2 / 2)
-    MSE.temp <- rep(NA,length(Z))
+    LLR.temp <- LLR(Z, data, h[i])
+    bias.temp <- LLR.temp[, (ncol(LLR.temp) - p + 1):ncol(LLR.temp)] * (h[i]^2 / 2)
+    MSE.temp <- numeric(n)
     
-    for (j in 1:length(Z)){
-      
-      pesi <- kernel(Z,Z[j],h[i])
+    for (j in 1:n) {
+      pesi <- kernel(Z[j], Z, h[i])
       est.density <- mean(pesi)
       
-      Y_fitted <- sum(X.matrix[j, ] * LLR.temp[j, 1:3])
-      e2 <- (data$Y - Y_fitted)^2
+      Y_fitted <- sum(X[j, ] * LLR.temp[j, 1:p])
+      e2 <- (Y - Y_fitted)^2
       
-      #e2 <- (data$Y - (LLR.temp[,1]*data$X1 + LLR.temp[,2]*data$X2 + LLR.temp[,3]*data$X3))^2
-      
-      N.temp <- N.e(var.l = X.matrix, pesi=pesi)
-      
-      inv_N <- tryCatch(solve(N.temp), error = function(e) return(NULL))
+      N.temp <- N.e(X, pesi)
+      inv_N <- tryCatch(solve(N.temp), error = function(e) NULL)
       if (is.null(inv_N)) {
         MSE.temp[j] <- NA
         next
       }
       
-      asy.temp <- asy.var(n,banda=h[i], N.matrix = N.temp ,e2,est.density,pesi=pesi)
+      asy.temp <- tryCatch(
+        asy.var(n, banda = h[i], N.matrix = N.temp, e2, est.density, pesi),
+        error = function(e) NULL
+      )
       if (is.null(asy.temp)) {
         MSE.temp[j] <- NA
         next
       }
-      MSE.temp[j] <- sum(diag(N.temp%*%asy.temp))*est.density + sum(bias.temp[j,]*(N.temp %*% bias.temp[j, ]))*est.density
+      
+      MSE.temp[j] <- sum(diag(N.temp %*% asy.temp)) * est.density +
+        sum(bias.temp[j, ] * (N.temp %*% bias.temp[j, ])) * est.density
     }
-    MSE[i] <- sum(MSE.temp,na.rm=T)
+    
+    MSE[i] <- sum(MSE.temp, na.rm = TRUE)
   }
+  
   return(MSE)
 }
 
