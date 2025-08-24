263e005a7a6a8a239fca52d9222674dadbbfc212
Luke-Data <garghelladuke@gmail.com>
2025-08-24 13:28:45 +0200

    Update KLASSO.R
    add NW cv optimal bandwidth

--- Model11/KLASSO.R
+++ Model11/KLASSO.R
@@ -14,16 +14,32 @@ n = 300
 X1 <- rnorm(n,mean = 2, sd = 1.5)
 X2 <- rbeta(n, shape1 = .5, shape2 = .5)
 X3 <- rgamma(n, shape = .5, rate = 1)
-Z1 <- runif(n, min = 0, max = 1)
+Z <- runif(n, min = 0, max = 1) # Z \in [0,1]
 
-Y <- X1*f1(Z1) + X2*f2(Z1) 
+Y <- X1*f1(Z) + X2*f2(Z) 
 
-data <- cbind(Y,X1,X2,X3,Z1)
+data <- cbind(Y,X1,X2,X3,Z)
 data <- as.data.frame(data)
+
+# set X.data active variable
+
+X.data <- as.data.frame(data[, !names(data) %in%  c("Y","Z")])
+
+# bw ottimale tramite cv
+
+library(np)
+
+bw <- npregbw(xdat = data.frame(data$Z),   # solo Z è variabile smoothing
+              ydat = data$Y,
+              zdat = X.data, 
+              regtype = "lc",  
+              bwmethod = "cv.ls")
+
+bw.opt <- bw$bw
 
 # starting coefficient function
 
-b.tilde <- NW(data$Y,data$Z,X.data,h=.4)
+b.tilde <- NW(data$Y,data$Z,X.data,h=bw.opt)
 
 # creating a lambda penalty grid value
 
@@ -112,10 +128,10 @@ for (i in 1:length(grid_lambda)){
 
 par(mfrow=c(1,2))
 
-plot(data$Z1,b.tilde[,3], type = 'l', ylim = c(-.1,.1), xlim = c(-0.05,1))
+plot(data$Z,b.tilde[,3], type = 'l', ylim = c(-.1,.1), xlim = c(-0.05,1))
 abline(h=0)
 
-plot(data$Z1,b.tilde.temp[,3], type = 'l', ylim = c(-.1,.1),xlim = c(-0.05,1))
+plot(data$Z,b.tilde.temp[,3], type = 'l', ylim = c(-.1,.1),xlim = c(-0.05,1))
 abline(h=0)
 
 
